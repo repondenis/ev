1fbcee9d3c4cd52a079785104b3b6ba2ace0cbef
Kudasov.SL <Kudasov.SL@WIN7-USER214>
2016-10-31 18:38:32 +0300

    fix

--- EVotingTestProjectMs/Pages/ForgotPasswordPage.cs
+++ EVotingTestProjectMs/Pages/ForgotPasswordPage.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HP.LFT.SDK.Web;
 
 namespace EVotingTestProjectMs.Pages
 {
-    class ForgotPasswordPage
+    class ForgotPasswordPage : Helpers.PageHelper
     {
+        private static CSSDescription description = new CSSDescription("div.wrapper>div>div>div.ng-scope");
+
+
+        public static bool isForgotPasswordPage()
+        {
+            return browser.Describe<IWebElement>(description).Exists() && browser.Describe<IWebElement>(description).GetVisibleText().Equals("Восстановление пароля");
+        }
     }
 }
