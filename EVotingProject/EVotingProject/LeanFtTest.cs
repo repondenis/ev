56a56b0db1eb4c4a0b6de8e02bd941daf01dcc93
repon <repon06@gmail.com>
2016-11-01 14:30:55 +0300

    fix

---
+++ EVotingProject/EVotingProject/LeanFtTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using HP.LFT.SDK;
+using HP.LFT.Verifications;
+
+namespace EVotingProject
+{
+    [TestFixture]
+    public class LeanFtTest : UnitTestClassBase
+    {
+
+
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+            // Setup once per fixture
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Before each test
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Clean up after each test
+        }
+
+        [OneTimeTearDown]
+        public void TestFixtureTearDown()
+        {
+            // Clean up once per fixture
+        }
+    }
+}
